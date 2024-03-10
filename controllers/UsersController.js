b663193081e5b4bc56cc9df1ce0da4b87351592f
pascallino <luvpascal.ojukwu@yahoo.com>
2024-03-10 10:21:51 +0100

    server.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,9 @@
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
 import Queue from 'bull';
 import dbClient from '../utils/db';
+// eslint-disable-next-line import/no-named-as-default
+import redisClient from '../utils/redis';
 
 const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
@@ -35,6 +38,25 @@ class UsersController {
         }).catch((error) => console.log(error));
       }
     });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      response.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
 
