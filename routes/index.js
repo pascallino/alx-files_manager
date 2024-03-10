df08009ee9004fb4f8882ed9729afc59d5de2068
pascallino <luvpascal.ojukwu@yahoo.com>
2024-03-10 16:35:16 +0100

    server.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -16,5 +17,7 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
